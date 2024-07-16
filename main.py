d330284aa8fcd0b33003861e2a000ce7ef4d0693
fh95-tech <fakih.hussein995@gmail.com>
2024-07-16 19:06:07 +0300

    adding add_driver function

--- main.py
+++ main.py
@@ -6,3 +6,27 @@ class Driver:
     self.startCity = startCity
     self.workerId = 'ID' +str(Driver.id).zfill(3) 
     Driver.id += 1
+
+cities=[]
+driversList = [Driver('max verstappen', 'akkar'),
+               Driver('charles leclerc', 'saida'),
+              Driver('lando norris', 'jbeil')]
+def add_driver():
+  name = input("Enter the driver's name: ")
+  while True:
+    startCity= input("Enter the driver's start city: ")
+    if startCity not in cities:
+      answer=input('The city is not available in the database,would you like to add it?(y/n) ')
+      if answer=='y':
+        cities.append(startCity)
+        print('City of ',startCity,'is added to the database')
+        print(cities)
+        break
+      elif answer=='n':
+        print('enter an existing city')
+      else:
+        print('enter a valid answer') 
+    else:
+      break     
+  driver = Driver(name.lower(), startCity.lower())
+  driversList.append(driver)
